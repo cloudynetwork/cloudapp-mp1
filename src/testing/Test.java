8e4f86e8c45b3e861ca3214a7e45e2c75ad3a9d6
Peter Joseph <peter@cloudynetwork.org>
2015-08-31 18:43:17 +0100

    Added inputs to the FileToArray() method. Filename and Encoding.

--- src/testing/Test.java
+++ src/testing/Test.java
@@ -1,46 +1,58 @@
 package testing;
+
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class Test {
 	
-	private String[] FileToArray() {
-		
-		
-		return 
-	}
 	
-    public String[] process() throws Exception {
-        String[] ret = new String[20];
-       
-		Scanner s = new Scanner(new File("input.txt"), "UTF-8");
+	/**
+	 * 
+	 * @param filename input file name string
+	 * @param encoding specify file encoding string, e.g UTF-8
+	 * @return Returns a String[] containing a line per element
+	 * @throws FileNotFoundException
+	 */
+	private String[] FileToArray(String filename, String encoding) throws FileNotFoundException {
+
+		Scanner s = new Scanner(new File(filename), encoding);
 
 		ArrayList<String> list = new ArrayList<String>(50);
-		
+
 		while (s.hasNextLine()) {
 			list.add(s.nextLine());
 		}
-      
-		s.close();
-		
-		System.out.println(list.size());
-		String[] filearray = new String[list.size()]; 
-		
-		list.toArray(filearray);
-		
-		System.out.println(filearray[108]);
-		
-       // Get indexes maybe use in loop ? Integer[] indexes = this.getIndexes();
 
-        // Set value of ret to output before return TO DO 
-        
-        return ret;
-    }
+		s.close();
+
+		System.out.println(list.size()); // For debugging
+		String[] filearray = new String[list.size()];
+
+		System.out.println(filearray[108]); // For debugging
+
+		return list.toArray(filearray);
+	}
+
+	public String[] process() throws Exception {
+		String[] ret = new String[20];
+
+		String[] tobeprocessed = FileToArray("input.txt", "UTF-8"); 
+
+		System.out.println(tobeprocessed[108]); // For debugging
+
+		
+		// Get indexes maybe use in loop ? Integer[] indexes =
+		// this.getIndexes();
+
+		// Set value of ret to output before return TO DO
+
+		return ret;
+	}
 
 	public static void main(String[] args) throws Exception {
-		
+
 		Test test1 = new Test();
 		String[] var1 = test1.process();
 
