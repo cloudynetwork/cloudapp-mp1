deabf08141785c6e2518fa000904c17efb535eff
Peter Joseph <peter@cloudynetwork.org>
2015-09-01 18:52:51 +0100

    Added my code to MP1. Odd double null returned on output...

--- src/MP1.java
+++ src/MP1.java
@@ -1,8 +1,19 @@
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Random;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 
 public class MP1 {
     Random generator;
@@ -48,6 +59,216 @@ public class MP1 {
         this.userName = userName;
         this.inputFileName = inputFileName;
     }
+    
+    /**
+	 * 
+	 * @param filename input file name string
+	 * @param encoding specify file 
+	 * @return Returns a String[] containing a line per element
+	 * @throws FileNotFoundException
+	 */
+	private String[] fileToArray(String filename, String encoding) throws FileNotFoundException {
+
+		Scanner s = new Scanner(new File(filename), encoding);
+
+		ArrayList<String> list = new ArrayList<String>(50);
+
+		while (s.hasNextLine()) {
+			list.add(s.nextLine());
+		}
+
+		s.close();
+
+//		System.out.println(list.size()); // For debugging
+		String[] filearray = new String[list.size()];
+
+		return list.toArray(filearray);
+	}
+	
+	/**
+	 * 
+	 * @param arraytoprocess String[] object to process
+	 * @param indexSize int of the index size to process
+	 * @return a String array of only the indexes provided by getIndexes()
+	 * @throws NoSuchAlgorithmException 
+	 */
+	private String[] extractMessageIndex(String[] arrayToProcess, int indexSize) throws NoSuchAlgorithmException {
+		
+		Integer[] processIndex = new Integer[indexSize];
+		processIndex = getIndexes();
+		
+		ArrayList<String> list = new ArrayList<String>(5000);
+		
+		for (int o: processIndex) {
+			
+			list.add(arrayToProcess[o]);
+		
+		}
+		
+		String[] returnArray = new String[list.size()];
+
+		return list.toArray(returnArray);
+	}
+
+	/**
+	 * 
+	 * @param arrayToProcess String[] with the elements including delimiters. 
+	 * @param delims a String[] with the desired delimiters to be removed. 
+	 * @return a String[] with the delimiters removed. 
+	 */
+	private String[] splitUsingDelimters(String[] arrayToProcess, String delims) {
+
+		ArrayList<String> list = new ArrayList<String>(10000);
+
+		for (String p : arrayToProcess) {
+
+			String[] toList = p.split(delims);
+			// Appears that default split does not remove leading trailing empty space
+
+			for (String q : toList) {
+				list.add(q);
+			}
+
+		}
+		
+		//Iterate through the ListArray and removes null and empty. 
+		Iterator<String> i = list.iterator();
+		while (i.hasNext()) {
+			String s = i.next();
+			if (s == null || s.isEmpty()) {
+				i.remove();
+			}
+		}
+		
+
+		String[] returnArray = new String[list.size()];
+		return list.toArray(returnArray);
+	}
+	
+	/**
+	 * Makes lowercase, removes whitespace and returns String[]
+	 * @param arrayToProcess
+	 * @return cleaned up String[]
+	 */
+	private String[] cleanUpArray(String[] arrayToProcess) {
+		
+		ArrayList<String> list = new ArrayList<String>(10000);
+		
+		for (String str : arrayToProcess) {
+			str = str.toLowerCase();
+			str = str.replace(" ", "");
+			list.add(str);
+		}
+		
+		String[] returnArray = new String[list.size()];
+		return list.toArray(returnArray);
+	}
+	
+	/**
+	 * 
+	 * @param arrayToProcess takes String[] array input of words to remove. 
+	 * @param stopWords takes String[] input of words to be removed. 
+	 * @return String[] with stopWords removed
+	 */
+	private String[] removeStopWords(String[] arrayToProcess, String[] stopWords) {
+
+		ArrayList<String> list = new ArrayList<String>(10000);
+
+		for (String str : arrayToProcess) {
+			list.add(str);
+		}
+
+		ArrayList<String> stopWordsList = new ArrayList<String>(100);
+
+		for (String str : stopWords) {
+			stopWordsList.add(str);
+		}
+
+		if (list.removeAll(stopWordsList) != true) {
+			System.out.println("removeStopWords Failed");
+		}
+
+		String[] returnArray = new String[list.size()];
+		return list.toArray(returnArray);
+
+	}
+	
+	private Map<String, Integer> countOccurrances(String[] arrayToCount) {
+
+		Map<String, Integer> m = new HashMap<String, Integer>();
+
+		for (String a : arrayToCount) {
+			Integer freq = m.get(a);
+			m.put(a, (freq == null ? 1 : freq + 1));
+
+		}
+
+//		System.out.println(m.size() + " distinct words:");
+//		System.out.println(m);
+		
+		m = sortMap(m);
+
+//		System.out.println(m.size() + " distinct words:");
+//		System.out.println(m);
+		
+		return m;
+
+	}
+	
+	private Map<String, Integer> sortMap(Map<String, Integer> m) {
+		
+		Map<String, Integer> sort = new TreeMap<String, Integer>(new MapComparator(m));
+		sort.putAll(m);
+		
+//		System.out.println(sort.size() + " distinct words:");
+//		System.out.println(sort);
+		return sort; 
+	}
+	
+	/**
+	 * 
+	 * @author 
+	 *
+	 */
+	class MapComparator implements Comparator<String> {
+
+		Map<String, Integer> m;
+
+		public MapComparator(Map<String, Integer> m) {
+			this.m = m;
+		}
+
+		public int compare(String a, String b) {
+			
+			Integer vala = m.get(a);
+			Integer valb = m.get(b);
+			
+			int result = valb.compareTo(vala);
+			
+			if (result == 0)
+				return a.compareTo(b);
+			else
+				return result;
+		}
+	}
+	
+	private String[] finalProcess(Map<String, Integer> inputMap) {
+
+		ArrayList<String> outputList = new ArrayList<String>(100);
+
+		for (String key : inputMap.keySet()) {
+//			System.out.println(key);
+			outputList.add(key);
+		}
+
+		String[] outputArray = new String[20];
+
+		for (int i = 0; i < 20; i++) {
+			outputArray[i] = outputList.get(i);
+		}
+
+		return outputArray;
+	}
 
     public String[] process() throws Exception {
         String[] ret = new String[20];
@@ -73,11 +294,31 @@ public class MP1 {
     	   System.out.println("Unable to read file: " + file.toString());
        }
       
-       // Get indexes maybe use in loop ? Integer[] indexes = this.getIndexes();
+       final String[] tobeprocessed = fileToArray("input.txt", "UTF-8"); 
 
-        // Set value of ret to output before return TO DO 
-        
-        return ret;
+		final String[] extractedIndicies = extractMessageIndex(tobeprocessed, 10000);
+			
+		final String[] delimitedArray = splitUsingDelimters(extractedIndicies, "([ \\t,;.?!\\-:@\\[\\](){}_*/])" ); 
+	    
+//		for (String key : delimitedArray) {
+//			System.out.println(key);
+//		}
+		
+		final String[] cleanedArray = cleanUpArray(delimitedArray);
+		
+		final String[] removedWords = removeStopWords(cleanedArray, stopWordsArray);
+		
+//		String[] testArray = {"valley","help","terrible","google","indium","varium","petrol","diesel","engine","iridium","elirium","gods","pest","famine","war","death","proof","darkness","valley","vb","vb","rainbow","rainbow","man","rainbow","valley","gods","valley","vb","vb","rainbow","rainbow","man","rainbow","valley","gods","valley","vb","vb","rainbow","rainbow","man","rainbow","valley","gods","valley","vb","vb","rainbow","rainbow","man","rainbow"};
+		
+		final Map<String, Integer> sortedMap = countOccurrances(removedWords);
+		
+		final String[] returnArray = finalProcess(sortedMap);
+
+		// Set value of ret to output before return TO DO
+		
+		ret = returnArray;
+		
+		return ret;
     }
 
     public static void main(String[] args) throws Exception {
